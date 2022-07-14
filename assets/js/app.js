9de1560ebd331b741ba60cb78012dc06d6da2e2e
Vazha Dzneladze <vaja.dzneladze@gmail.com>
2022-07-14 13:12:48 +0400

    js : update service worker url

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ console.log('App js is ready to use ...')
 
 if('serviceWorker' in navigator){
 
-    navigator.serviceWorker.register('https://vajadzneladze.github.io/serviceWorker/')
+    navigator.serviceWorker
+    .register('https://vajadzneladze.github.io/serviceWorker/sw.js')
     .then(() => console.log('Service Worker Registered'))
 }
