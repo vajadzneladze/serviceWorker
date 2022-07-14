9e22519f197e56ea976bfd4491946df48d28d239
Vazha Dzneladze <vaja.dzneladze@gmail.com>
2022-07-14 04:40:10 +0400

    html: fix bug , js : service Worker

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
 console.log('App js is ready to use ...')
+
+
+if('serviceWorker' in navigator){
+
+    navigator.serviceWorker.register('https://vajadzneladze.github.io/serviceWorker/')
+    .then(() => console.log('Service Worker Registered'))
+}
---
+++ sw.js
@@ -0,0 +1,46 @@
+importScripts('assets/js/cache-polyfill.js');
+
+let CACHE_VERSION =  'v1';
+let CACHE_FILES = [
+    './',
+    'index.html'
+]
+
+selft.addEventListener('install', e => {
+
+    selft.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE_VERSION)
+        .then(() => {
+            console.log(' Cache Opened');
+            return caches.addAll(CACHE_FILES);
+        })
+    )
+})
+
+
+self.addEventListener('fetch', e => {
+    let online = navigator.online;
+    if(!online) {
+        e.respondWith(
+            caches.match(e.request).then( res => {
+
+                if(res){
+                    return res;
+                }
+            })
+        )
+    }
+})
+
+
+self.addEventListener('activate', e => {
+
+    e.waitUntil(
+        caches.keys().then( (keys, i) => {
+            if(keys !== CACHE_VERSION){
+                return caches.delete(keys[i])
+            }
+        })
+    )
+})
